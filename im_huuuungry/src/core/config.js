e8cb86a0edb808e91b03d50c5e5c43319c253066
Daniel Guddemi <danguddemi@pop-os.localdomain>
2020-03-28 18:36:37 -0400

    Recreating app with Expo and Firebase Auth

---
+++ im_huuuungry/src/core/config.js
@@ -0,0 +1,11 @@
+// Replace with your own firebase config!
+export const FIREBASE_CONFIG = {
+  apiKey: "AIzaSyCja3RuYyPfm3-F1yxRg35P0TRV6OXqKFY",
+  authDomain: "sesame-chicken.firebaseapp.com",
+  databaseURL: "https://sesame-chicken.firebaseio.com",
+  projectId: "sesame-chicken",
+  storageBucket: "sesame-chicken.appspot.com",
+  messagingSenderId: "915181843372",
+  appId: "1:915181843372:android:a9af75ccbddc1d390d8009",
+  measurementId: "G-226834771"
+};
